3eaec62cbd2027528faade4b11263d870fec161b
carmebe <carmebe@uc.cl>
2024-06-07 03:26:12 -0400

    fix: example data fetch

--- src/utils/randomData.js
+++ src/utils/randomData.js
@@ -1,10 +1,10 @@
 export async function getRandomProject() {
     let randomNumber = Math.floor(Math.random() * 100) + 1;
-    const response = await fetch('http://dummyjson.com/users/'+randomNumber);
+    const response = await fetch('https://dummyjson.com/users/'+randomNumber);
     const data = await response.json();
     return data.firstName + ' ' + data.lastName;
   }
-  
+
   export async function getRandomTask() {
     const response = await fetch('https://dummyjson.com/todos/random');
     const data = await response.json();
